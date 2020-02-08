886be5bcca490cdf689cb834f095bad22c89237f
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-08 01:25:11 +0100

    feat: serve persons members on GET /id

--- index.js
+++ index.js
@@ -30,6 +30,18 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 app.get("/info", (req, res) => {
   const message =
     `<p>Phonebook has info for ${persons.length} people</p>` +
