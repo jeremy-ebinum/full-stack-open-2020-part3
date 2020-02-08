83edccb7579133a33bf398a25a8a3d79d74d6478
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-08 12:55:11 +0100

    chore: prepare for inital heroku deployment
    - add Procfile
    - add node version in package.json "engines"
    - set PORT to first check env variable

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ app.use((err, req, res, next) => {
   next();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
