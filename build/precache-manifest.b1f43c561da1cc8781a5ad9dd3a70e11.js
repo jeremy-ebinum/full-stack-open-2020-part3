8de8e40a412a2c5cd9b22d01ec161d52c248a97c
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-08 13:44:57 +0100

    uibuild

---
+++ build/precache-manifest.b1f43c561da1cc8781a5ad9dd3a70e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4530ae151b0c313a574333a7b0e7691",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab2c3bcd4e3b164eba91",
+    "url": "/static/css/main.3a210263.chunk.css"
+  },
+  {
+    "revision": "1c0f03c047f65f7af66f",
+    "url": "/static/js/2.83e1e5e6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.83e1e5e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab2c3bcd4e3b164eba91",
+    "url": "/static/js/main.84ca8ad9.chunk.js"
+  },
+  {
+    "revision": "73cc5c71f716e545cadb",
+    "url": "/static/js/runtime-main.a70bf60b.js"
+  }
+]);
